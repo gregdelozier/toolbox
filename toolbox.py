eea6d34fd2fd177c0e741f203d36f477f677763f
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-12-07 11:08:18 -0800

    Initial toolbox server

---
+++ toolbox.py
@@ -0,0 +1,17 @@
+# Toolbox web server
+
+import os
+from bottle import default_app, run, route, static_file, redirect
+
+@route('/')
+def get_index():
+    redirect("/index.html")
+
+@route('<filepath:path>')
+def get_file(filepath):
+    return static_file(filepath, root="./")
+
+if "PYTHONANYWHERE_DOMAIN" in os.environ:
+    application = default_app()
+else:
+    run(host='localhost', port=8080)
